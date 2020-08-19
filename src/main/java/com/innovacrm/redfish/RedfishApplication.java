1117bbfe22b1c2c4e039a5a2de5dd41d22748e09
Reyad Khan <reyadk2@gmail.com>
2020-08-20 00:04:32 +0600

    initial setup

---
+++ src/main/java/com/innovacrm/redfish/RedfishApplication.java
@@ -0,0 +1,13 @@
+package com.innovacrm.redfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedfishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedfishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innovacrm/redfish/RedfishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovacrm.redfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedfishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
